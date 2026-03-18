7bcdcf0fc2ed1e43c8ea99b02907a591e9834ffe
elyeskallel24-creator <elyeskallel24@gmail.com>
2026-03-18 13:06:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -165,7 +165,13 @@ else:
         st.caption("LyceeAI v1.0 | Quantara-SPMAT") 
     # --- SIDEBAR END ---
 
-    st.title("🎓 LyceeAI") 
+    # This creates two invisible columns: one big empty one, and one for the logo
+    col_empty, col_logo = st.columns([8, 2])
+    with col_logo:
+        # This writes LyceeAI in bold, white, and aligned to the right
+        st.markdown("<h1 style='text-align: right; color: white;'><b>LyceeAI</b></h1>", unsafe_allow_html=True)
+
+    st.title("👨🏻‍🏫 OstedhiAI") 
     if "messages" not in st.session_state: st.session_state.messages = [] 
     
     # Show history 
