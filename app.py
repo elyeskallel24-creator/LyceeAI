0fc8445f000f4737f8c199a3c964a5c0ea647b5a
elyeskallel24-creator <elyeskallel24@gmail.com>
2026-03-25 10:16:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -202,21 +202,21 @@ else:
             with st.container(border=True):
                 st.markdown("### <span style='font-weight: normal; color: grey;'>LyceeAI</span> **LITE**", unsafe_allow_html=True)
                 st.write("OstedhiAI yefhem fil program el tounsi")
-                st.markdown("<span style='font-size: 32px; font-weight: bold;'>8dt</span> <span style='font-size: 18px; color: grey;'>/month</span>", unsafe_allow_html=True)
+                st.markdown("<span style='font-size: 32px; font-weight: bold;'>8dt</span> <span style='font-size: 18px; color: grey;'>/mois</span>", unsafe_allow_html=True)
         # 3. Putting a rounded empty box in the second column
         with col2:
             # Adding a title in the second empty rounded box
             with st.container(border=True):
                 st.markdown("### <span style='font-weight: normal; color: grey;'>LyceeAI</span> **PLUS**", unsafe_allow_html=True)
                 st.write("LITE + OstedhiAI yjewbek asra3 + historique akber")
-                st.markdown("<span style='font-size: 32px; font-weight: bold;'>26dt</span> <span style='font-size: 18px; color: grey;'>/month</span>", unsafe_allow_html=True)
+                st.markdown("<span style='font-size: 32px; font-weight: bold;'>26dt</span> <span style='font-size: 18px; color: grey;'>/mois</span>", unsafe_allow_html=True)
         # 4. Putting a rounded empty box in the third column
         with col3:
             # Adding a title in the third empty rounded box
             with st.container(border=True):
                 st.markdown("### <span style='font-weight: normal; color: grey;'>LyceeAI</span> **PRO**", unsafe_allow_html=True)
                 st.write("LITE + PLUS + Accès kemel lmizet LyceeAI lkol + AI a9wa (mémoire et vitesse)")
-                st.markdown("<span style='font-size: 32px; font-weight: bold;'>125dt</span> <span style='font-size: 18px; color: grey;'>/month</span>", unsafe_allow_html=True)
+                st.markdown("<span style='font-size: 32px; font-weight: bold;'>125dt</span> <span style='font-size: 18px; color: grey;'>/mois</span>", unsafe_allow_html=True)
     elif st.session_state.page == "chat":
         # THIS IS YOUR ORIGINAL CHATBOT CODE
         st.title("👨🏻‍🏫 OstedhiAI") 
